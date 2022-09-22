9aee714d68bc1a61e5ed4d53b295aadae2ebbab8
Bhaumik Patel <bhaumikpatel@users.noreply.github.com>
2022-09-22 15:37:53 +0530

    menu toggle issue fixed bs5

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -12,8 +12,8 @@ const TopMenu = () => {
           <button
             className="navbar-toggler"
             type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
